23fdbb5cd6c0c050a53c1e3098f39b59a2cae9d4
Aditya Agarwal <aditya81070@gmail.com>
2019-01-18 22:53:18 +0530

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import ListContacts from './ListContacts'
 
 class App extends Component {
+  state = {
+    contacts: [{
+      'id': 'karen',
+      'name': 'Karen Isgrigg',
+      'handle': 'karen_isgrigg',
+      'avatarURL': 'http://localhost:5001/karen.jpg'
+    },
+    {
+      'id': 'richard',
+      'name': 'Richard Kalehoff',
+      'handle': 'richardkalehoff',
+      'avatarURL': 'http://localhost:5001/richard.jpg'
+    },
+    {
+      'id': 'tyler',
+      'name': 'Tyler McGinnis',
+      'handle': 'tylermcginnis',
+      'avatarURL': 'http://localhost:5001/tyler.jpg'
+    }
+    ]
+  }
+
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
+        <ListContacts
+          contacts={this.state.contacts} 
+          onDeleteContact={this.removeContact}/>
       </div>
     )
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function ListContacts (props) {
+  return (
+    <ol className='contact-list'>
+      {
+        props.contacts.map((contact) => (
+          <li key={contact.id} className='contact-list-item'>
+            <div
+              className='contact-avatar'
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }} />
+            <div
+              className='contact-details'>
+              <p>{contact.name}</p>
+              <p>@{contact.handle}</p>
+            </div>
+            <button className='contact-remove'>
+                  Remove
+            </button>
+          </li>
+        ))
+      }
+    </ol>
+  )
+}
+
+export default ListContacts
