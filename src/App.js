abfebb8411eb0debe276e57e9fbf12dd7386c273
Aditya Agarwal <aditya81070@gmail.com>
2019-01-25 06:17:49 +0530

    App.js: remove local data and use lifecycle event to fetch the data from api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    contacts: [{
-      'id': 'karen',
-      'name': 'Karen Isgrigg',
-      'handle': 'karen_isgrigg',
-      'avatarURL': 'http://localhost:5001/karen.jpg'
-    },
-    {
-      'id': 'richard',
-      'name': 'Richard Kalehoff',
-      'handle': 'richardkalehoff',
-      'avatarURL': 'http://localhost:5001/richard.jpg'
-    },
-    {
-      'id': 'tyler',
-      'name': 'Tyler McGinnis',
-      'handle': 'tylermcginnis',
-      'avatarURL': 'http://localhost:5001/tyler.jpg'
-    }
-    ]
+    contacts: []
   }
 
+  componentDidMount () {
+    ContactsAPI.getAll()
+      .then((contacts) => this.setState({ contacts }))
+  }
   removeContact = (contact) => {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter((c) => c.id !== contact.id)
