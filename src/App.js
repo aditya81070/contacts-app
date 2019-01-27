6a981431f16abbcfdea8a68990ae4d907e2f779b
Aditya Agarwal <aditya81070@gmail.com>
2019-01-27 21:06:24 +0530

    App.js: add Route to list and create contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
@@ -20,9 +22,12 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact} />
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact} />
+        )} />
+        <Route path='/create' component={CreateContact} />
       </div>
     )
   }
